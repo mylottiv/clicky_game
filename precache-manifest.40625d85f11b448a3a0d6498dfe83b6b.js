5686b6939fc6842c2ebbd1a1f147ef1e4619987b
Thomas Mylott <mylottiv@gmail.com>
2020-01-17 17:52:59 -0600

    Updates

---
+++ precache-manifest.40625d85f11b448a3a0d6498dfe83b6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aea3b2fc8ad88f6f03a93c30bb23c36",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "768e5bd16e3062b09dcd",
+    "url": "/clicky_game/static/js/2.e57e223e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky_game/static/js/2.e57e223e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb9e054e3e6280d19def",
+    "url": "/clicky_game/static/js/main.4dd777c5.chunk.js"
+  },
+  {
+    "revision": "78667b5d6623909d0094",
+    "url": "/clicky_game/static/js/runtime-main.76cb44ae.js"
+  }
+]);
